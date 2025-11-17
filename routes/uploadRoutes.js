b20a4716f06859f94ddbad924bbf852a60850efd
B yss <20240031277@my.xu.edu.ph>
2025-11-17 09:41:52 +0800

    cloudinary testing

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -24,4 +24,9 @@ router.post("/", upload.single("image"), (req, res) => {
   });
 });
 
+console.log("Cloud name:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("API key:", process.env.CLOUDINARY_API_KEY);
+console.log("API secret:", process.env.CLOUDINARY_API_SECRET);
+
+
 export default router;
