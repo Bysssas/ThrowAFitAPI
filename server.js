85618a69aa4bd53eb43904be73e43a7b78ab615d
B yss <20240031277@my.xu.edu.ph>
2025-11-18 19:57:06 +0800

    api connections

--- server.js
+++ server.js
@@ -4,18 +4,34 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import itemRoutes from "./routes/itemRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 dotenv.config();
 console.log("MONGO_URI:", process.env.MONGO_URI);
+
 connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
 app.use("/api/items", itemRoutes);
 app.use("/api/upload", uploadRoutes);
+
+// Root route for testing
+app.get("/", (req, res) => {
+  res.send("Throw-A-Fit API is running");
+});
+
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
